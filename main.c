e5247585e920ed0212bcfd3af2c03bb078b0b49b
Mohamed Echane <mechane@e2r8p4.1337.ma>
2023-02-25 09:45:49 +0100

    ok

--- main.c
+++ main.c
@@ -2,30 +2,46 @@
 #include<pthread.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include<sys/time.h>
 
-int i;
-void  *func()
+pthread_t philosopher[4];
+pthread_mutex_t forks[4];
+
+void  *func(void *val)
 {
-	printf("thread number %d\n",i);
-	return(0);
+	int n = (long)val;
+	printf("Philosoper  %d is thinking \n",n);
+	pthread_mutex_trylock(&forks[n]);
+	pthread_mutex_trylock(&forks[n + 1]);
+	printf("philosopher %d is eating\n",n);
+	usleep(3);
+	pthread_mutex_unlock(&forks[n]);
+	pthread_mutex_unlock(&forks[n + 1]);
+	printf("philosopher %d is going to sleep\n",n);
+	usleep(10);
+	return(NULL);
 }
 int main()
 {
-	pthread_t	t[5];
-	i = 0;
+	int i = 0;
 
-	while(i < 5)
+	while (i < 4)
 	{
-		if(pthread_create(t + i, NULL, &func, NULL) != 0)
+		pthread_mutex_init(&forks[i],NULL);
+		i++;
+	}
+	i = 0;
+	while(i < 4)
+	{
+		if(pthread_create(&philosopher[i], NULL, &func, (void*)(long)i) != 0)
 			return(perror("Failed to create thread !!"),1);
 		i++;
 	}
 	i = 0;
-	while(i < 5)
+	while(i < 4)
 	{
-		if(pthread_join(t[i], NULL) != 0)
+		if(pthread_join(philosopher[i], NULL) != 0)
 			return(2);
-		printf("thread number : %d finished \n",i);
 		i++;
 	}
 	return(0);
