c5c6d7eaca40de900660b0a4c8cd20afc2cbda27
Mohamed Echane <mechane@e2r9p3.1337.ma>
2023-02-27 14:44:06 +0100

    hk

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
-#include<stdio.h>
-#include<pthread.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include<sys/time.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mechane <mechane@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/27 14:41:38 by mechane           #+#    #+#             */
+/*   Updated: 2023/02/27 14:41:39 by mechane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
 
 pthread_t philosopher[4];
 pthread_mutex_t forks[4];
@@ -10,21 +18,30 @@ pthread_mutex_t forks[4];
 void  *func(void *val)
 {
 	int n = (long)val;
-	printf("Philosoper  %d is thinking \n",n);
-	pthread_mutex_trylock(&forks[n]);
-	pthread_mutex_trylock(&forks[n + 1]);
-	printf("philosopher %d is eating\n",n);
-	usleep(3);
-	pthread_mutex_unlock(&forks[n]);
-	pthread_mutex_unlock(&forks[n + 1]);
-	printf("philosopher %d is going to sleep\n",n);
-	usleep(10);
+		while(1)
+		{
+		if (n % 2)
+			usleep(2);
+		printf("Philosoper  %d is thinking \n",n);
+		usleep(100);
+		pthread_mutex_lock(&forks[n]);
+		printf("Philosoper  %d has taken a fork \n",n);
+		pthread_mutex_trylock(&forks[n + 1]);
+		printf("Philosoper  %d has taken a fork \n",n);
+		printf("Philosopher %d is eating\n",n);
+		usleep(10000);
+		pthread_mutex_unlock(&forks[n]);
+		pthread_mutex_unlock(&forks[n + 1]);
+		printf("Philosopher %d is going to sleep\n",n);
+		usleep(10000);
+		}
 	return(NULL);
 }
 int main()
 {
 	int i = 0;
 
+	
 	while (i < 4)
 	{
 		pthread_mutex_init(&forks[i],NULL);
@@ -40,9 +57,10 @@ int main()
 	i = 0;
 	while(i < 4)
 	{
-		if(pthread_join(philosopher[i], NULL) != 0)
+		if(pthread_detach(philosopher[i]) != 0)
 			return(2);
 		i++;
 	}
+	while(1);
 	return(0);
 }
---
+++ philosopher.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mechane <mechane@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/27 14:41:34 by mechane           #+#    #+#             */
+/*   Updated: 2023/02/27 14:43:39 by mechane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHER_H
+#define PHILOSOPHER_H 
+
+#include<stdio.h>
+#include<pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include<sys/time.h>
+
+
+
+
+
+
+
+#endif
